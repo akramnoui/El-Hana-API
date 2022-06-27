a0fbbcdc482da2e61d20a41d21b3ec755a40a06c
Akram Noui <im_noui@esi.dz>
2022-06-27 01:07:55 +0100

    added SSL support

--- queries/config.js
+++ queries/config.js
@@ -30,6 +30,9 @@ const proConfig = process.env.DATABASE_URL; //heroku addons
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
+    ssl: {    /* <----- Add SSL option */
+    rejectUnauthorized: false,
+  }
 });
 
 module.exports = pool;
